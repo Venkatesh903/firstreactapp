fe0cd1c3a419bf73f05d93e51423632c2873f55c
Venkatesh903 <123933394+Venkatesh903@users.noreply.github.com>
2024-09-01 21:39:37 +0530

    forms

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>First React App</p>
+        <form>
+          <label>Enter your name:
+            <input type="text" placeholder='name'/>
+          </label><br></br>
+          <label>Enter your Employee Id:
+            <input type="text" placeholder='id'/>
+          </label><br></br>
+          <label>Enter your Base Location:
+            <input type="text" placeholder='location'/>
+          </label>
+        </form>
       </header>
     </div>
   );
